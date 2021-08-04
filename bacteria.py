4916e13683ffc2d94223860bbbad9c0bbf58f629
Pablo Bobo Student <201600206@alu.comillas.edu>
2021-08-04 21:20:17 +0200

    Creación de función plot_colony_growth
    Se añade una nueva característica a bacteria que establece el tamaño de la colonia. También se crea la función plot_colony_growth que muestra con una serie de círculos el aumento de individuos en una colonia de tamaño inicial 1, velocidad de crecimiento 1 y coordenadas en el origen.

--- bacteria.py
+++ bacteria.py
@@ -1,8 +1,14 @@
 # Este archivo servirá como referencia para crear/manejar clases en python
 
-class bacteria() :
-    def __init__(self, bac_name, col_name, growth_rate=1, x=0, y=0, colour='yellow'):
-        # Cada colonia de bacterias tendrá el nombre de la especie, el nombre de la colonia, una velocidad de
+from math import e
+from math import pi
+from math import sqrt
+import matplotlib.pyplot as plt
+
+
+class bacteria():
+    def __init__(self, bac_name, col_name, growth_rate=1, x=0, y=0, ini_pop=1, colour='yellow'):
+        # Cada colonia de bacterias tendrá el nombre de la especie, el nombre de la colonia, la velocidad de
         # propagación, coordenadas iniciales de colonia y color asociado a la colonia. Por defecto la velocidad de
         # propagación será igual a 1, el color será amarillo y se encontrará en el origen.
         self.bac_name = bac_name
@@ -10,6 +16,7 @@ class bacteria() :
         self.growth_rate = growth_rate
         self.x = x
         self.y = y
+        self.ini_pop = ini_pop
         self.colour = colour
 
     def change_col_name(self, new_name):
@@ -24,4 +31,23 @@ class bacteria() :
         # Función para cambiar el color de la colonia
         self.colour = new_colour
 
+
+def bacteria_growth_formula(pop_ini, time, growth_rate):
+    pop = pop_ini * float(e) ** (growth_rate * time)
+    return pop
+
+def plot_colony_growth():
+    # Esta función simple plotea una colonia de bacterias en coordenadas (0,0) con una velocidad de crecimiento de 1
+    figure, axes = plt.subplots()
+    axes.set_aspect(1)
+    time = input("Introduzca en segundos la duración del cultivo: ")
+
+    for i in range(0, int(time)):
+        area = bacteria_growth_formula(1, time=i, growth_rate=1)
+        rad = sqrt(area / pi)
+        draw_circle = plt.Circle((0, 0), rad, color='red', alpha=0.2)
+        axes.add_artist(draw_circle)
+        plt.title('Bacteria')
+
+    plt.show()
 
